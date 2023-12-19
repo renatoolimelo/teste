cce50942768cb70208753701b59923548f1468f6
Renato Oliveira de Melo <renato.olimelo@gmail.com>
2023-12-19 14:31:18 -0300

    Alterando sh

--- teste.sh
+++ teste.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+timestamp_teste=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
+
+chmod 775 /opt/yaman/teste/jmeter.jmx
+
 cp /opt/yaman/teste/jmeter.jmx /opt/yaman/scripts
 
-/opt/yaman/jmeter/apache-jmeter-5.5/bin/jmeter -n -t /opt/yaman/scripts/jmeter.jmx -l /opt/yaman/outputs/resultado.csv
+/opt/yaman/jmeter/apache-jmeter-5.5/bin/jmeter -j jmeter_${timestamp_teste}.log -n -t /opt/yaman/scripts/jmeter.jmx -l /opt/yaman/outputs/resultado_${timestamp_teste}.csv
 
-cp /opt/yaman/outputs/resultado.csv /opt/yaman/teste
+cp /opt/yaman/outputs/resultado_${timestamp_teste}.csv /opt/yaman/teste
+cp /opt/yaman/outputs/jmeter_${timestamp_teste}.log /opt/yaman/teste
 
 cd /opt/yaman/teste
 
 git config --global user.email "renato.olimelo@gmail.com"
 git config --global user.name "Renato Melo"
 
-git add resultado.csv
+git add resultado_${timestamp_teste}.csv jmeter_${timestamp_teste}.log
 
-git commit -m "resultado.csv"
+git commit -m "resultado_${timestamp_teste}.csv"
 
-git push https://renatoolimelo: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@github.com/renatoolimelo/teste.git --all
+git push https://${GIT_USER}:${GIT_PASSWORD}@github.com/renatoolimelo/teste.git --all
 
 exit 0
-
