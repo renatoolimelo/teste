b6ec55866154750f3ab345ea31a6a09e47ea208f
Renato Oliveira de Melo <renato.olimelo@gmail.com>
2023-12-20 14:15:32 -0300

    Solicitando nome do robo

--- teste.sh
+++ teste.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+ echo parametros errados
+ echo use: $0 "<nome_do_robo>"
+ exit 1
+fi
+
+NOME_ROBO=$1
+
 #setando timestamp
 timestamp_teste=`date '+%Y-%m-%d_%Hh%Mm%Ss'`
 
@@ -14,13 +23,13 @@ chmod 775 /opt/yaman/teste/jmeter.jmx
 cp /opt/yaman/teste/jmeter.jmx /opt/yaman/scripts
 
 #executando Jmeter
-/opt/yaman/jmeter/apache-jmeter-5.5/bin/jmeter -j /opt/yaman/outputs/jmeter_${timestamp_teste}.log -n -t /opt/yaman/scripts/jmeter.jmx -l /opt/yaman/outputs/resultado_${timestamp_teste}.csv
+/opt/yaman/jmeter/apache-jmeter-5.5/bin/jmeter -j /opt/yaman/outputs/jmeter_${timestamp_teste}.log -n -t /opt/yaman/scripts/${NOME_ROBO}.jmx -l /opt/yaman/outputs/${NOME_ROBO}_${timestamp_teste}.csv
 
 #copiando o resultado o jmeter.log para a pasta do GIT
 if [ ! -d "/opt/yaman/teste/resultado" ]; then
 	mkdir /opt/yaman/teste/resultado
 fi
-cp /opt/yaman/outputs/resultado_${timestamp_teste}.csv /opt/yaman/teste/resultado
+cp /opt/yaman/outputs/${NOME_ROBO}_${timestamp_teste}.csv /opt/yaman/teste/resultado
 cp /opt/yaman/outputs/jmeter_${timestamp_teste}.log /opt/yaman/teste/resultado
 
 #indo para o diretorio do GIT
@@ -31,8 +40,8 @@ git config --global user.email "renato.olimelo@gmail.com"
 git config --global user.name "Renato Melo"
 
 #Subindo o arquivo para o git hub
-git add ./resultado/resultado_${timestamp_teste}.csv ./resultado/jmeter_${timestamp_teste}.log
-git commit -m "resultado_${timestamp_teste}.csv"
+git add ./resultado/${NOME_ROBO}_${timestamp_teste}.csv ./resultado/jmeter_${timestamp_teste}.log
+git commit -m "${NOME_ROBO}_${timestamp_teste}.csv"
 git push https://${GIT_USER}:${GIT_PASSWORD}@github.com/renatoolimelo/teste.git --all
 
 exit 0
